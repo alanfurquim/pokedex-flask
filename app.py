b4bbbebac7e947a6841f96eafb13b999d2d6512c
Alan Furquim <alan.furquim002@gmail.com>
2025-02-06 23:03:06 -0300

    80percent

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
+import json
+import requests
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+def carregar_pokemons():
+    with open('secrets.key') as file:
+        psw = file.read()
+
+    reqUrl = "https://alanfurquim.pythonanywhere.com/dados"
+
+    headersList = {
+        "x-api-key": psw
+    }
+
+    response = requests.request("GET", reqUrl,  headers=headersList, timeout=5).json()
+
+    return {key: response[key] for key in sorted(response.keys(), key=int)}
+
+
 @app.route('/')
 def home():
-    return render_template('index.html')
+    pokemons = carregar_pokemons()
+    return render_template('index.html', pokemons=pokemons)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,15 @@
-function mostrarMensagem() {
-    alert("Você clicou no botão!");
+function filterPokemon() {
+    let searchText = document.getElementById("search-bar").value.toLowerCase();
+    let pokemonCards = document.querySelectorAll(".pokemon-card");
+
+    pokemonCards.forEach(card => {
+        let name = card.querySelector(".pokemon-name").textContent.toLowerCase();
+        let types = Array.from(card.querySelectorAll(".pokemon-type")).map(type => type.textContent.toLowerCase());
+
+        if (name.includes(searchText) || types.some(type => type.includes(searchText))) {
+            card.style.display = "block";
+        } else {
+            card.style.display = "none";
+        }
+    });
 }
